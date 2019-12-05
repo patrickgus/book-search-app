0fb53782d4d3cca0cdd29435c171511117bcf6de
Patrick Gustafson <patrickgus@charter.net>
2019-12-05 15:07:17 -0800

    add static version of Book

---
+++ src/BookList/Book/Book.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Book.css";
+
+export default function Book(props) {
+  return (
+    <div className="book">
+      <h2>{props.title}</h2>
+      <div className="book-data">
+        <img src={props.thumbnail} alt="book cover"/>
+        <div className="book-info">
+          <p>Author: {props.author}</p>
+          <p>Price: {this.price}</p>
+          <p className="description">{props.description}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
